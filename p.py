c988dd6b00610daa100be987b922fe6ebb5652ae
Donna Co <chuhuilin228068@gmail.com>
2018-07-21 11:07:46 +0800

    add products

---
+++ p.py
@@ -0,0 +1,15 @@
+products = []
+while True:
+	name = input('請輸入商品名稱:')
+	if name == 'q':
+		break
+	price = input('請輸入商品價格:')
+	p = []
+	p.append(name) #小清單
+	p.append(price)	#小清單
+	#也可以將7-9行直接縮成: p = [name, price]
+	products.append(p) #大清單 (小清單中裝入大清單)
+	#又或者直接縮成products.append([name, price])
+print(products)
+
+products[0][0] #大清單的第0格，小清單的第0格
