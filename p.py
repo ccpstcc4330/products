63d5e791bca901ce0cfa300ee2347c28921c70ee
Donna Co <chuhuilin228068@gmail.com>
2018-07-23 11:23:39 +0800

    add header

--- p.py
+++ p.py
@@ -4,6 +4,7 @@ while True:
 	if name == 'q':
 		break
 	price = input('請輸入商品價格:')
+	price = int(price)
 	p = []
 	p.append(name) #小清單
 	p.append(price)	#小清單
@@ -16,3 +17,14 @@ print(products)
 
 for p in products:
 	print(p[0], '的價格是$', p[1])
+
+#把使用者資料存住
+with open('products.csv', 'w', encoding = 'utf-8') as f: 
+#寫入模式，所以沒有products.txt也沒關係
+# 修正亂碼問題，要加encoding = 'utf-8'，讓語言可被讀取
+#用csv存取檔案，可以用excel開啟(直接開excel中文字跑不出，所以:
+#資料-取得外部資料-從文字檔選UTF-8。分隔符號用逗點)
+	f.write('商品, 價格\n')
+	for p in products:
+		f.write(p[0] + ',' + str(p[1]) +'\n') #字串可以做 + 跟 *
+# 但是＋只能用在字串加字串，或者整數加整數。所以利用str轉成字串
